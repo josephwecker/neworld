57c24390039ac257549259e9992b5930761e74fe
Joseph Wecker <jwecker@173-116-18-29.pools.spcsdns.net>
2010-10-26 13:28:10 -0700

    Polish- no resizing glitches, normalized some code, and escaped fullscreen if not available.

--- src/ViewPort.hx
+++ src/ViewPort.hx
@@ -6,6 +6,8 @@ import Utils;
  * directly- only has main_bitmap display to the screen.
  */
 class ViewPort extends flash.display.Sprite {
+    var portal_width(get_portal_width, null) : Int;
+    var portal_height(get_portal_height, null) : Int;
     var view_data : BitmapData;
     var main_bitmap : Bitmap;
 
@@ -16,32 +18,41 @@ class ViewPort extends flash.display.Sprite {
         flash.Lib.current.cacheAsBitmap =   true;
         stage.scaleMode =                   flash.display.StageScaleMode.NO_SCALE;
         stage.align =                       flash.display.StageAlign.TOP_LEFT;
-        flash.Lib.current.contextMenu =     null;
+        stage.showDefaultContextMenu=       false;
+        flash.Lib.current.contextMenu =     new flash.ui.ContextMenu();
+        flash.Lib.current.contextMenu.hideBuiltInItems();
         flash.Lib.current.mouseEnabled =    false;
+
+        main_bitmap = new Bitmap(null, flash.display.PixelSnapping.ALWAYS, false);
+        calculate_boundaries(null);
+        flash.Lib.current.addChild(main_bitmap);
 
         addEventListener(flash.events.Event.ENTER_FRAME, draw_current_state);
         stage.addEventListener(flash.events.Event.RESIZE, calculate_boundaries);
         stage.addEventListener(flash.events.KeyboardEvent.KEY_DOWN, maybe_fullscreen);
-
-        view_data = new BitmapData(stage.stageWidth, stage.stageHeight, true, 0);
-        main_bitmap = new Bitmap(view_data);
-        flash.Lib.current.addChild(main_bitmap);
     }
 
+    inline function get_portal_width() : Int { return stage.stageWidth;}
+    inline function get_portal_height(): Int { return stage.stageHeight;}
+
     function calculate_boundaries(_) {
-        main_bitmap.width = stage.stageWidth;
-        main_bitmap.height = stage.stageHeight;
-        view_data = new BitmapData(stage.stageWidth, stage.stageHeight, true, 0);
+        view_data = new BitmapData(portal_width, portal_height, true, 0);
         main_bitmap.bitmapData = view_data;
+        stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+        stage.align = flash.display.StageAlign.TOP_LEFT;
+        main_bitmap.width = portal_width;
+        main_bitmap.height = portal_height;
     }
 
     function maybe_fullscreen(e : flash.events.KeyboardEvent) {
-        if(e.keyCode == 70) {
-            trace("Toggling fullscreen");
-            if(stage.displayState == flash.display.StageDisplayState.NORMAL)
-                stage.displayState = flash.display.StageDisplayState.FULL_SCREEN;
-            else stage.displayState = flash.display.StageDisplayState.NORMAL;
-        }
+        try {
+            if(e.keyCode == 70) {
+                trace("Toggling fullscreen");
+                if(stage.displayState == flash.display.StageDisplayState.NORMAL)
+                    stage.displayState = flash.display.StageDisplayState.FULL_SCREEN;
+                else stage.displayState = flash.display.StageDisplayState.NORMAL;
+            }
+        } catch(_:Dynamic) {}
     }
 
     function draw_current_state(_) {
