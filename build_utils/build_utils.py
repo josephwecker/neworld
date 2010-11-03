32187995e351cf45528a0c122fb6ea2f1cbe2b17
Joseph Wecker <jwecker@justin.tv>
2010-11-03 14:34:34 -0700

    Fleshing out build a little more

--- build_utils/build_utils.py
+++ build_utils/build_utils.py
@@ -19,6 +19,25 @@ for command in sys.argv:
         DEBUG = True
         sys.argv.remove('-debug')
 
+def flatten(l, ltypes=(list, tuple)):
+    ltype = type(l)
+    l = list(l)
+    i = 0
+    while i < len(l):
+        while isinstance(l[i], ltypes):
+            if not l[i]:
+                l.pop(i)
+                i -= 1
+                break
+            else:
+                l[i:i + 1] = l[i]
+        i += 1
+    return ltype(l)
+
+sources = ['./', 'src', 'lib']
+setup(dirs=flatten([sources,'swfs/']), runner='smart_runner',
+        hasher=mtime_hasher)
+
 def p(*args):
     '''Shorthand for joining path segments together into one path'''
     return os.path.normpath(os.path.join(*args))
@@ -26,12 +45,25 @@ def p(*args):
 BUILD_UTIL_DIR = os.path.abspath(os.path.dirname(__file__) + '/')
 ROOT_DIR = p(BUILD_UTIL_DIR, '..')
 
+HAXE_LIBS = []
+HAXE_RESOURCES = []
+
 def mkdir(*path):
     '''Shorthand for mkdir -p and then any number of path segments.'''
     run('mkdir', '-p', p(*path))
 
 def rmdir(*path):
     run('rm', '-rf', p(*path))
+
+def haxe(name):
+    cmd = ['haxe',
+            '-main', name,
+            [['-cp',s] for s in sources],
+            '-swf9', 'swfs/'+ name.lower() + '.swf',
+            '-swf-version', 10,
+            '-debug' if DEBUG else None]
+    run(cmd)
+    #   print flatten(cmd)
 
 # Make sure we're in the right working directory to begin with (possibly not
 # needed and possibly even interferes w/ fabricate later, we'll see)
