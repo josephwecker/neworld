40d5facee08347368641aee6e7516e18f72d0b54
Joseph Wecker <jwecker@JTVs-MacBook-Pro.local>
2010-11-03 12:13:06 -0700

    Playing with a new, hopefully faster world map generation algorithm

---
+++ src/STest.hx
@@ -0,0 +1,24 @@
+import Simplex;
+import haxe.Timer;
+
+class STest {
+    static public function main() {
+        haxe.Log.trace = function(v:Dynamic,?i){flash.Lib.trace("STest: "+v);};
+        var stest = new STest();
+    }
+
+    public function new() {
+        flash.Lib.current.cacheAsBitmap =   true;
+        flash.Lib.current.stage.scaleMode =                   flash.display.StageScaleMode.NO_SCALE;
+        flash.Lib.current.stage.align =                       flash.display.StageAlign.TOP_LEFT;
+        trace("start\n");
+        var time = Timer.stamp();
+        var noise = new SimplexNoise(200, 20, 0.5);
+        var bmd = new flash.display.BitmapData(500,500);
+        noise.noise2D(bmd);
+        var bm = new flash.display.Bitmap(bmd);
+        flash.Lib.current.addChild(bm);
+        time = Timer.stamp() - time;
+        trace("end ("+time+")\n");
+    }
+}
