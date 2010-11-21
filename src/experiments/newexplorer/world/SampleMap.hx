f0e2df724d7db09627de1baeeb8dd96a1f9753a2
Joseph Wecker <jwecker@174-144-244-222.pools.spcsdns.net>
2010-11-20 16:36:24 -0800

    Bigger world, decreased octaves

--- src/experiments/newexplorer/world/SampleMap.hx
+++ src/experiments/newexplorer/world/SampleMap.hx
@@ -18,7 +18,7 @@ class SampleMap {
     static var BASE =        0.002 / (1 << 4);
     static var GAIN =        1.0;
     static var OFFSET =      0.735;
-    static var OCTAVES =     5;
+    static var OCTAVES =     4;
     static var PERSISTENCE = 0.9;
     static var SEED =        156;
     static var X =           390 * (1 << 4);
@@ -35,15 +35,15 @@ class SampleMap {
     }
 
     function fill_maps() {
-        var bmd = new BitmapData(100, 100, false, 0);
+        var bmd = new BitmapData(400, 400, false, 0);
         rp.fill(bmd, X, Y, 0, BASE, GAIN, OFFSET, true);
         data.push(bmd);  // Base bedrock height
         
         for(i in 1...3) {
-            bmd = new BitmapData(100, 100, false, 0);
+            bmd = new BitmapData(400, 400, false, 0);
             rp.fill(bmd, X, Y, 0, BASE + (i / 100), GAIN, OFFSET - (i / 5), true);
             data.push(bmd);
-            bmd = new BitmapData(100, 100, false, 0);
+            bmd = new BitmapData(400, 400, false, 0);
             rp.fill(bmd, X, Y, 0, BASE + (i / 75), GAIN, OFFSET - (i / 7.5), true);
             data.push(bmd);
         }
