554be3182dc39ae94b167f2b3ccd48a3774f4fd0
Samuel Wecker <wecker.samuel@gmail.com>
2010-12-13 17:32:42 -0800

    Reaction delay works.

--- server/simulation/src/orc.erl
+++ server/simulation/src/orc.erl
@@ -14,6 +14,7 @@ birth(Name, World) ->
 
 live(World, Me = [Self, Name, X, Y, State]) ->
   receive
+
     {Him, {birth, _}} ->
       [Target, HisName, HisX, HisY, HisState] = Him, 
       DistX = HisX - X,
@@ -21,29 +22,26 @@ live(World, Me = [Self, Name, X, Y, State]) ->
       Dist = math:sqrt((DistX * DistX) + (DistY * DistY)),
       case Dist < 5 of
         true ->
-          react(threaten(Self,Target));
+          react_time(random:uniform(5000)),
+          Target ! {Me, {threaten, "You're dead!!"}},
+          say(Name, HisName, "You're dead!!");
         false -> cant_see
       end;
+
     {Him, {threaten, Msg}} ->
       [Target, HisName, HisX, HisY, HisState] = Him, 
-      react(threaten_back(Self,Target))
+      react_time(random:uniform(5000)),
+      Target ! {Me, {threaten_back, "No you're dead!!~n"}},
+      say(Name, HisName, "No You're dead!!!")
+
   end,
   live(World, Me).
-
-threaten(Me, Him) ->
-  [Self,   Name,    X,    Y,    State   ] = Me, 
-  [Target, HisName, HisX, HisY, HisState] = Him, 
-  Target ! {Self, {threaten, "You're dead!!"}},
-  say(Name, HisName, "You're dead!!").
-
-threaten_back(Me, Him) ->  
-  [Self,   Name,    X,    Y,    State   ] = Me, 
-  [Target, HisName, HisX, HisY, HisState] = Him, 
-  Target ! {Self, {threaten_back, "No you're dead!!~n"}},
-  say(Name, HisName, "No You're dead!!!").
 
 say(From, To, Msg) ->
   io:format("~s -> ~s: ~s~n",[From, To, Msg]).
 
-react(F) ->
-  timer:apply_after(random:uniform(5000),F).
+react_time(Time) ->
+  receive
+    after Time ->
+        true
+    end.
